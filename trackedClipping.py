7b7cbc8b30c0005d5a031349ceb3d879fab8cc8a
Daniel Lincoln <dlincol1@yahoo.com>
2022-03-29 11:02:12 -0400

    Update trackedClipping.py
    Changed from vrAEBase to QTimer

--- trackedClipping.py
+++ trackedClipping.py
@@ -1,52 +1,89 @@
-class trackerTools(vrAEBase):
-    ''' VR Tools for the Vive Tracker '''
+from PySide2.QtCore import QTimer
+from PySide2.QtGui import QVector3D
+    
+class xrClippingTools:
+    # Clipping tools for Tracker devices
     
     def __init__(self):
-        vrAEBase.__init__(self)
-        self.addLoop()
-
+        self.timer = QTimer()
+        self.invertClipDirection = False
+        self.clipAxis('6DoF')
+        
         # Get the first tracker device
         self.tracker = vrDeviceService.getVRDevice("tracker-1")
 
-        # Create a Transform3D node with a geometry plane as child
+        # Create a Transform3D node with a plane as child
         self.trackerTransform = vrNodeService.findNode("trackerTransform")
         
         if not self.trackerTransform.isValid():
             self.trackerTransform = createNode("Transform3D", "trackerTransform", False)
             
             # Create a plane for Clipping position to follow and enable Z axis adjustments
-            self.clipPlane = createPlane(25.0, 25.0, 1, 1, 1.0, 0.0, 0.0)
-            self.clipPlane.setName('clipPlane')
-            self.trackerTransform.addChild(self.clipPlane)
+            self.clipPlaneGeo = createPlane(25.0, 25.0, 1, 1, 1.0, 0.0, 0.0)
+            self.clipPlaneGeo.setName('clipPlane')
+            self.trackerTransform.addChild(self.clipPlaneGeo)
         else:
-            self.clipPlane = findNode('clipPlane')
+            self.clipPlaneGeo = findNode('clipPlane')
         
         # Constrain Transform3D node to the tracker
         self.trackerConstraint = vrConstraintService.createParentConstraint(
             [self.tracker.getNode()], self.trackerTransform, False)
         
-        self.setActive(True)
-        vrLogInfo('trackerTools activated')
-        
+        vrLogInfo('xrClippingTools activated.')
+    
     def __del__(self):
         vrLogInfo('Destructor called, deleting constraint.')
         vrConstraintService.deleteConstraint(self.trackerConstraint)
-        
-    def updateClippingPosition(self):            
-        # Get World Transform of the geometry plane
-        matrix = self.clipPlane.getWorldTransform()
-            
-        # Note, change Vec3f parameters to 1,0,0 for X-axis clipping
-        setClippingPlane(Pnt3f(matrix[3], matrix[7], matrix[11]),
-                         Vec3f(matrix[2], matrix[6], matrix[10]), invertClipDirection)
     
-    def recEvent(self, state):
-        vrAEBase.recEvent(self, state)
-         
-    def loop(self):
-        if self.isActive():
-            self.updateClippingPosition()
+    def clipFlip(self):
+        clip_direction = getClippingPlaneInvertDirection()
+        
+        if (clip_direction):
+            self.invertClipDirection = False
+        else:
+            self.invertClipDirection = True
+
+    def clipStart(self):
+        if self.timer.isActive():
+            return
+        self.timer.start()
+
+    def clipStop(self):
+        self.timer.stop()
+        vrLogInfo('Clip update stopped.')
+        
+    def clip6DoF(self):            
+        # Get World Transform of the geometry plane
+        matrix = self.clipPlaneGeo.getWorldTransform()
+        setClippingPlane(Pnt3f(matrix[3], matrix[7], matrix[11]),
+                         Vec3f(matrix[2], matrix[6], matrix[10]), self.invertClipDirection)
+
+    def clipX(self):            
+        matrix = self.clipPlaneGeo.getWorldTransform()
+        setClippingPlane(Pnt3f(matrix[3], matrix[7], matrix[11]),
+                         Vec3f(1, 0, 0), self.invertClipDirection)
+
+    def clipY(self):            
+        matrix = self.clipPlaneGeo.getWorldTransform()
+        setClippingPlane(Pnt3f(matrix[3], matrix[7], matrix[11]),
+                         Vec3f(0, 1, 0), self.invertClipDirection)
+                         
+    def clipZ(self):            
+        matrix = self.clipPlaneGeo.getWorldTransform()
+        setClippingPlane(Pnt3f(matrix[3], matrix[7], matrix[11]),
+                         Vec3f(0, 0, 1), self.invertClipDirection)
+                         
+    def clipAxis(self, axis):
+        if (axis == '6DoF'):
+            self.timer.timeout.connect(self.clip6DoF)
+        elif (axis == 'X'):
+            self.timer.timeout.connect(self.clipX)
+        elif (axis == 'Y'):
+            self.timer.timeout.connect(self.clipY)
+        elif (axis == 'Z'):
+            self.timer.timeout.connect(self.clipZ)
+
 
 enableClippingPlane(True)
-invertClipDirection = True
-tracker_tools = trackerTools()
+xrclip = xrClippingTools()
+xrclip.clipStart()
